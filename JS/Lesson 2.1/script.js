376813e15ed0e3042cea4caa185d053e435010da
Valera <74453468+VaBeus@users.noreply.github.com>
2021-02-26 23:29:08 +0300

    Add files via upload

---
+++ JS/Lesson 2.1/script.js
@@ -0,0 +1,6 @@
+
+'use strict';
+let userName = prompt('Укажите Ваше имя, пожалуйста.', ['Valera']);
+alert(`Ваше имя - ${userName} !`);
+let trueName = confirm('Ваше имя вывелось правильно?');
+alert(trueName);
