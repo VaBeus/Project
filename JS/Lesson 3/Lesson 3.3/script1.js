577b391f791427a998eff3185b3cf113330f98ce
Valera <74453468+VaBeus@users.noreply.github.com>
2021-03-09 12:20:08 +0300

    Add files via upload

---
+++ JS/Lesson 3/Lesson 3.3/script1.js
@@ -0,0 +1,24 @@
+let number = 4;
+let margin = 5;
+let result = (1024 - margin * 2 * number)/number;
+alert(result)
+
+let count =+ prompt("INPUT NUMBER");
+let width =+ prompt("input width");
+number = 1024/(width + 2 * margin);
+
+
+let sum = count/number;
+let mod = count-sum * number;
+
+alert(`${Math.floor(number)} ${Math.floor(sum)} ${Math.floor(mod)}`); 
+
+
+
+
+
+
+
+
+
+
